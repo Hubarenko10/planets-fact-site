2e244841df923e334960a54eb5c1f81c5084d664
Ivan Hubarenko <103453171+Hubarenko10@users.noreply.github.com>
2023-09-16 12:13:20 +0300

    ed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import GlobalStyles from 'components/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='planets-fact-site'>
+    <BrowserRouter basename='planets-fact-site/'>
     <GlobalStyles/>
     <App />
     </BrowserRouter>
